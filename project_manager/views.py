a671802f6dc13ba8c5e4282b4914d7f92fa3d189
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-16 19:21:21 +0200

    add is_complited check

--- project_manager/views.py
+++ project_manager/views.py
@@ -1,5 +1,7 @@
+from typing import Any
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
@@ -16,3 +18,7 @@ class ProjectActiveListView(LoginRequiredMixin, generic.ListView):
     model = Project
     context_object_name = "project_completed_list"
     template_name = "pages/index.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = Project.objects.filter(is_complited=False)
+        return queryset
