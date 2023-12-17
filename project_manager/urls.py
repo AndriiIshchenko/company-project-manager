b8f7b1c284e10cfb23941c8ef91c45d125a130d3
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-17 17:04:04 +0200

    add path for TaskDetailView

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProjectActiveListView, ProjectDetailView
+from .views import ProjectActiveListView, ProjectDetailView, TaskDetailView
 
 urlpatterns = [
     path("", ProjectActiveListView.as_view(), name="index"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("taskss/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
 ]
 app_name = "project_manager"
