78adca85a622f2b364507b6df9b0840d8eca087d
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-17 16:59:03 +0200

    add path for ProjectDetailView

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProjectActiveListView
+from .views import ProjectActiveListView, ProjectDetailView
 
 urlpatterns = [
     path("", ProjectActiveListView.as_view(), name="index"),
+    path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
 ]
 app_name = "project_manager"
