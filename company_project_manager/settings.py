b3dc6997e8b8cbcc8c5d42fdf7076da9050f891e
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-18 13:44:57 +0200

    add settings for crispy and bootstrap

--- company_project_manager/settings.py
+++ company_project_manager/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "project_manager",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
