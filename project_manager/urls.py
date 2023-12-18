235dbe5f2ee753908e34dddd0f0241dd06735390
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-18 14:04:28 +0200

    add path for TaskCreate

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/cretate/", TaskCreateView.as_view(), name="task-create"),
 ]
 app_name = "project_manager"
