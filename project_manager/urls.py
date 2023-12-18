a2b9187dc9c617557f3cb5f7ce7d34adbf82a940
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-18 10:33:31 +0200

    add path for TaskUpdate

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProjectActiveListView, ProjectDetailView, TaskDetailView
+from project_manager.views import ProjectActiveListView, ProjectDetailView, TaskDetailView, TaskUpdateView
 
 urlpatterns = [
     path("", ProjectActiveListView.as_view(), name="index"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 app_name = "project_manager"
