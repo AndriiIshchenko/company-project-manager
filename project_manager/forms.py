8ef64da7858b37a816b225c1003c00d15e9a5502
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-19 15:42:47 +0200

    add ProjectForm

--- project_manager/forms.py
+++ project_manager/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from project_manager.models import Project, Task
+from project_manager.models import Project, Task, Worker
 
 
 class TaskForm(forms.ModelForm):
@@ -16,4 +16,12 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         widgets = {"project": forms.HiddenInput()}
+        fields = "__all__"
+
+
+class ProjectForm(forms.ModelForm):
+    assigness = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.filter(position__name__icontains="project manager"))
+    class Meta:
+        model = Project
         fields = "__all__"
