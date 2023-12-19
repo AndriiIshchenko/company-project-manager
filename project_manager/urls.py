a415ab700a7c94857d8dc466b93079a4d7083f5a
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-19 10:45:26 +0200

    add path for Project?CreateView

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from project_manager.views import (
     ProjectActiveListView,
+    ProjectCreateView,
     ProjectDetailView,
     TaskCreateView,
     TaskDetailView,
@@ -11,6 +12,7 @@ from project_manager.views import (
 urlpatterns = [
     path("", ProjectActiveListView.as_view(), name="index"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("projects/create/", ProjectCreateView.as_view(), name="project-create"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/cretate/", TaskCreateView.as_view(), name="task-create"),
