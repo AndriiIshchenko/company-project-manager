0bc4357fc2c78044a3b485d95d0ee18f89c5777f
Andrii Ishchenko <iscenkoa@gmail.com>
2023-12-22 17:47:41 +0200

    add path for ProjectComplited

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from project_manager.views import (
     ProjectActiveListView,
+    ProjectCompletedListView,
     ProjectCreateView,
     ProjectDetailView,
     TaskCreateView,
@@ -15,6 +16,7 @@ from project_manager.views import (
 
 urlpatterns = [
     path("", ProjectActiveListView.as_view(), name="index"),
+    path("projects/completed/", ProjectCompletedListView.as_view(), name="project-completed"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
     path("projects/create/", ProjectCreateView.as_view(), name="project-create"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
